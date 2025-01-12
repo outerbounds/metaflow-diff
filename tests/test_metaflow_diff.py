44ce36dac2631bedd8b5b9e70910995358f9ad75
Nissan Pow <npow@netflix.com>
2025-01-11 20:40:53 -0800

    fix tests

--- tests/test_metaflow_diff.py
+++ tests/test_metaflow_diff.py
@@ -34,7 +34,7 @@ class TestMetaflowDiff(unittest.TestCase):
         self.assertTrue(os.path.exists(tmp.name))
 
     @patch("metaflow_diff.metaflow_diff.run")
-    def test_perform_diff(self, mock_run):
+    def test_perform_diff_output_false(self, mock_run):
         with tempfile.TemporaryDirectory() as source_dir, tempfile.TemporaryDirectory() as target_dir:
             source_file = os.path.join(source_dir, "file.txt")
             target_file = os.path.join(target_dir, "file.txt")
@@ -43,10 +43,57 @@ class TestMetaflowDiff(unittest.TestCase):
             with open(target_file, "w") as f:
                 f.write("target content")
 
-            perform_diff(source_dir, target_dir)
+            perform_diff(source_dir, target_dir, output=False)
+
+            # if output=False, run should be called twice:
+            # 1. git diff
+            # 2. less -R
+            assert mock_run.call_count == 2
+
+            mock_run.assert_any_call(
+                [
+                    "git",
+                    "diff",
+                    "--no-index",
+                    "--exit-code",
+                    "--no-color",
+                    "./file.txt",
+                    source_file,
+                ],
+                text=True,
+                stdout=PIPE,
+                cwd=target_dir,
+            )
+
+            mock_run.assert_any_call(["less", "-R"], input=mock_run().stdout, text=True)
+
+    @patch("metaflow_diff.metaflow_diff.run")
+    def test_perform_diff_output_true(self, mock_run):
+        with tempfile.TemporaryDirectory() as source_dir, tempfile.TemporaryDirectory() as target_dir:
+            source_file = os.path.join(source_dir, "file.txt")
+            target_file = os.path.join(target_dir, "file.txt")
+            with open(source_file, "w") as f:
+                f.write("source content")
+            with open(target_file, "w") as f:
+                f.write("target content")
+
+            perform_diff(source_dir, target_dir, output=True)
+
+            assert mock_run.call_count == 1
 
             mock_run.assert_called_once_with(
-                ["git", "diff", "--no-index", source_file, target_file],
+                [
+                    "git",
+                    "diff",
+                    "--no-index",
+                    "--exit-code",
+                    "--no-color",
+                    "./file.txt",
+                    source_file,
+                ],
+                text=True,
+                stdout=PIPE,
+                cwd=target_dir,
             )
 
     @patch("shutil.rmtree")
@@ -72,7 +119,7 @@ class TestMetaflowDiff(unittest.TestCase):
 
             op_patch(tmpdir, patch_file)
 
-            mock_perform_diff.assert_called_once_with(tmpdir, os.getcwd(), output=True)
+            mock_perform_diff.assert_called_once_with(tmpdir, output=True)
             with open(patch_file, "r") as f:
                 content = f.read()
             self.assertIn("diff --git a/file.txt b/file.txt\n", content)
